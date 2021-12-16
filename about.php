ab29b30d40a599e64ce4134d3e066b68515d09e3
scruncho <60299363+scruncho@users.noreply.github.com>
2021-12-15 23:14:59 -0500

    ne

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?php 
-    $title = 'Index';
+    $title = 'About';
     require_once 'includes/header.php'; 
     require_once 'database/conn.php'; 
 ?>
--- feature.php
+++ feature.php
@@ -1,6 +1,6 @@
 
 <?php 
-    $title = 'Index';
+    $title = 'Gallery';
     require_once 'includes/header.php'; 
     require_once 'database/conn.php'; 
 ?>
