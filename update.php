527c1c9ee51165dd0bb6f4b9a520b03eaff3ef04
scruncho <60299363+scruncho@users.noreply.github.com>
2021-12-21 21:17:42 -0500

    Update update.php

--- update.php
+++ update.php
@@ -17,59 +17,52 @@ if (!isset($_GET['id'])) {
 ?>
 <div class="container p-md-4">
 
-<center><img src="images/logo.png" style="width:150px;height:150px;"></center>
-<h1 class="text-center">UPDATE RECORD</h1>
+    <center><img src="images/logo.png" style="width:150px;height:150px;"></center>
+    <h1 class="text-center">UPDATE RECORD</h1>
 
-<form method="post" action="editpost.php" enctype="multipart/form-data">
+    <form method="post" action="editpost.php" enctype="multipart/form-data">
 
-    <input type="hidden" name="id" value="<?php echo $member['members_id'] ?>" />
+        <input type="hidden" name="id" value="<?php echo $member['members_id'] ?>" />
 
-    <div class="form-group">
-        <label for="firstname">First Name</label>
-        <!-- <input required type="text" class="form-control" id="firstname" name="firstname"> -->
-        <input type="text" class="form-control" value="<?php echo $member['firstname'] ?>" id="firstname" name="firstname">
-    </div>
-    <div class="form-group">
-        <label for="lastname">Last Name</label>
-        <!-- <input required type="text" class="form-control" id="lastname" name="lastname"> -->
-        <input type="text" class="form-control" value="<?php echo $member['lastname'] ?>" id="lastname" name="lastname">
-    </div>
-
-
-    <div class="form-group">
-        <label for="Email">Email address</label>
-        <!-- <input required type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"> -->
-        <input readonly type="text" class="form-control" value="<?php echo $member['emailaddress'] ?>" id="emailaddress" name="emailaddress">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-
-    <div class="form-group">
-        <label for="gender">Gender</label>
-        <select class="form-control" id="gender" name="gender">
-            <option><?php echo $member['gender'] ?></option>
-            <option> Male</option>
-            <option>Female</option>
-        </select>
-    </div>
-
-    <div class="form-group">
-        <label for="address">Address</label>
-        <!-- <input type="text" class="form-control" id="address" name="address" aria-describedby="phoneHelp"> -->
-        <input type="text" class="form-control" value="<?php echo $member['address'] ?>" id="address" name="address">
-        <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
-    </div>
-
-    <!-- <div class="custom-file">
-
-        <input type="file" accept="image/*" class="custom-file-input" src="<?php // echo $member['avatar_path'] ?>" id="avatar_path" name="avatar_path">
-        <label class="custom-file-label" for="avatar">Choose File</label>
-        <small id="avatar" class="form-text text-danger">File Upload is Optional</small>
-
-    </div> -->
+        <div class="form-group">
+            <label for="firstname">First Name</label>
+            <!-- <input required type="text" class="form-control" id="firstname" name="firstname"> -->
+            <input type="text" class="form-control" value="<?php echo $member['firstname'] ?>" id="firstname" name="firstname">
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last Name</label>
+            <!-- <input required type="text" class="form-control" id="lastname" name="lastname"> -->
+            <input type="text" class="form-control" value="<?php echo $member['lastname'] ?>" id="lastname" name="lastname">
+        </div>
 
 
-    <button type="submit" name="submit" class="btn btn-primary" class="btn-block" style="width:100%">Save Changes</button>
-</form>
+        <div class="form-group">
+            <label for="Email">Email address</label>
+            <!-- <input required type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"> -->
+            <input readonly type="text" class="form-control" value="<?php echo $member['emailaddress'] ?>" id="emailaddress" name="emailaddress">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+
+        <div class="form-group">
+            <label for="gender">Gender</label>
+            <select class="form-control" id="gender" name="gender">
+                <option><?php echo $member['gender'] ?></option>
+                <option> Male</option>
+                <option>Female</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="address">Address</label>
+            <!-- <input type="text" class="form-control" id="address" name="address" aria-describedby="phoneHelp"> -->
+            <input type="text" class="form-control" value="<?php echo $member['address'] ?>" id="address" name="address">
+            <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
+        </div>
+
+
+
+        <button type="submit" name="submit" class="btn btn-primary" class="btn-block" style="width:100%">Save Changes</button>
+    </form>
 </div>
 <br>
 <br>
